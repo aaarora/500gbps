5f9d419376d0e45f029a917992d202e7b187cd07
Jonathan Guiang jkguiang@gmail.com 8/2/2017 <jguiang@uaf-10.t2.ucsd.edu>
2021-11-15 18:09:54 -0800

    fixed

--- docker/image-config.d/mntnvmes.sh
+++ docker/image-config.d/mntnvmes.sh
@@ -3,9 +3,9 @@
 nvmes=($(nvme list | awk '{print $1}'))
 for i in ${!nvmes[@]}; do
     if [ "$i" -gt "2" ]; then
-        $i=$(( i - 2 ))
-        mkdir /nvme${i}
-        mount -t xfs ${nvmes[$i]} /nvme${i}
+        j=$(( i - 2 ))
+        mkdir /nvme${j}
+        mount -t xfs ${nvmes[$i]} /nvme${j}
     fi
 done
 chown xrootd:xrootd /nvme*
