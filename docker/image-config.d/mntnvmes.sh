dfc7be70138a042c27dd8b2f6330689f7f5f75f0
Jonathan Guiang jkguiang@gmail.com 8/2/2017 <jguiang@uaf-10.t2.ucsd.edu>
2021-11-15 18:02:57 -0800

    fixed

--- docker/image-config.d/mntnvmes.sh
+++ docker/image-config.d/mntnvmes.sh
@@ -3,6 +3,8 @@
 nvmes=($(nvme list | awk '{print $1}'))
 for i in ${!nvmes[@]}; do
     if [ "$i" -gt "2" ]; then
+        $i=$(( i - 2 ))
+        mkdir /nvme${i}
         mount -t xfs ${nvmes[$i]} /nvme${i}
     fi
 done
