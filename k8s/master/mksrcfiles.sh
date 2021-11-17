5cb8e7a9935ff3f10dea6aabda93781da06bdaee
jkguiang <jguiang@ucsd.edu>
2021-11-17 12:27:10 -0800

    fixed

--- k8s/master/mksrcfiles.sh
+++ k8s/master/mksrcfiles.sh
@@ -7,7 +7,7 @@ count="0"
 pods=($(kubectl get pods | awk '{print $1}'))
 for pod in $pods; do
     if [[ "$pod" == *"src-origin"* ]]; then
-        kubectl exec $pod -- bash /home/mkfiles.sh $count $numTransfers &
+        kubectl exec $pod -- bash /home/mkfile.sh $count $numTransfers &
         count=$[$count+$numTransfer]
     fi
 done
