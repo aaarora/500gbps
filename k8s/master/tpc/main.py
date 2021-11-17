a56e3ce5b0c141e18eb3aef66b8af2994f187fc4
jkguiang <jguiang@ucsd.edu>
2021-11-16 16:51:20 -0800

    updated to submit batches

--- k8s/master/tpc/main.py
+++ k8s/master/tpc/main.py
@@ -6,11 +6,13 @@ import logging
 import sys
 
 def main(args) -> None:
-  logging.basicConfig(filename='transfer.log', 
-  filemode='w', 
-  level=logging.INFO, 
-  format='%(asctime)s  %(levelname)s - %(message)s', 
-  datefmt='%Y%m%d %H:%M:%S')
+  logging.basicConfig(
+    filename='transfer.log', 
+    filemode='w', 
+    level=logging.INFO, 
+    format='%(asctime)s  %(levelname)s - %(message)s', 
+    datefmt='%Y%m%d %H:%M:%S'
+  )
 
   if not checkSocket(args.source, args.destination, port=args.port):
     sys.exit(1)
@@ -24,6 +26,6 @@ if __name__ == "__main__":
   parser.add_argument('--destination', type=str, help='Dest Server')
   parser.add_argument('--numTransfers', type=int, help='# of Transfers')
   parser.add_argument('--numStreams', type=int, help='# of Streams')
-  parser.add_argument('--port', type=int, help='XRootD Port', default=2094)
+  parser.add_argument('--port', type=int, help='XRootD Port', default=9001)
   args = parser.parse_args()
   main(args)
--- k8s/master/tpc/scheduler.py
+++ k8s/master/tpc/scheduler.py
@@ -2,18 +2,20 @@ import logging
 import asyncio
 
 from aiomultiprocess import Pool
+from math import ceil
 
 class TransferScheduler:
-  def __init__(self, source, destination, xrdport: int, numTransfers: int, numStreams: int):
+  def __init__(self, source, destination, xrdport: int, numTransfers: int, numBatches: int, numStreams: int):
     self.source = source
     self.destination = destination
     self.xrdport = xrdport
     self.numTransfers = numTransfers
+    self.numBatches = numBatches
     self.numStreams = numStreams
   
-  def makeTransferQueue(self):
+  def makeTransferQueue(self, start, stop):
     logging.info("Building queue...")
-    for num in range(self.numTransfers):
+    for num in range(start, stop):
       logging.debug(f"Added {num}/{self.numTransfers} transfers to queue")
       cmd = ['curl', '-L', '-X', 'COPY']
       cmd += ['-H', 'Overwrite: T']
@@ -28,18 +30,23 @@ class TransferScheduler:
   async def worker(cmd) -> None:
     while True:
       process = await asyncio.create_subprocess_exec(
-      *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        *cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+      )
       
       stdout, stderr = await process.communicate()
       result = stdout.decode().strip()
       print(result)
 
-  async def runTransfers(self) -> None:
-    queue = self.makeTransferQueue()
+  async def runTransfers(self, start, stop) -> None:
+    queue = self.makeTransferQueue(start, stop)
 
     logging.info("Starting Transfers")
     async with Pool(processes=4) as pool:
-        await pool.map(self.worker, queue)
+      await pool.map(self.worker, queue)
 
   def startTransfers(self) -> None:
-    asyncio.run(self.runTransfers())
+    batchSize = ceil(self.numTransfers/self.numBatches)
+    for b in range(self.numBatches):
+      start = b*batchSize
+      stop = min(self.numTransfers, (b+1)*batchSize)
+      asyncio.run(self.runTransfers(start, stop))
