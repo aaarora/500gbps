5712aee7cd7076aa0b37a4e89f990d28e54d327e
jkguiang <jguiang@ucsd.edu>
2021-11-17 12:23:51 -0800

    updated for new source server names

---
+++ k8s/master/mksrcfiles.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+numTransfers="38"
+numBatchess="4"
+count="0"
+
+pods=($(kubectl get pods | awk '{print $1}'))
+for pod in $pods; do
+    if [[ "$pod" == *"src-origin"* ]]; then
+        kubectl exec $pod -- bash /home/mkfiles.sh $count $numTransfers &
+        count=$[$count+$numTransfer]
+    fi
+done
