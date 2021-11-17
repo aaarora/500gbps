66428dc1b2f5840ab4060a0f06a7a73e7f5bd79a
jkguiang <jguiang@ucsd.edu>
2021-11-17 12:31:48 -0800

    added numBatches to CLI

--- k8s/master/tpc/main.py
+++ k8s/master/tpc/main.py
@@ -17,7 +17,14 @@ def main(args) -> None:
   if not checkSocket(args.source, args.destination, port=args.port):
     sys.exit(1)
   
-  tsched = TransferScheduler(args.source, args.destination, args.port, args.numTransfers, args.numStreams)
+  tsched = TransferScheduler(
+    args.source, 
+    args.destination, 
+    args.port, 
+    args.numTransfers, 
+    ags.numBatches, 
+    args.numStreams
+  )
   tsched.startTransfers()
 
 if __name__ == "__main__":
@@ -25,6 +32,7 @@ if __name__ == "__main__":
   parser.add_argument('--source', type=str, help='Source Server')
   parser.add_argument('--destination', type=str, help='Dest Server')
   parser.add_argument('--numTransfers', type=int, help='# of Transfers')
+  parser.add_argument('--numBatches', type=int, help='# of Batches (of Transfers)')
   parser.add_argument('--numStreams', type=int, help='# of Streams')
   parser.add_argument('--port', type=int, help='XRootD Port', default=9001)
   args = parser.parse_args()
